e0c6a238b1181f08c10f8ba28c6ddd4b732b7a84
Aleks V <phenomen@pm.me>
2022-03-31 11:41:16 -0500

    Initial commit

---
+++ src/components/DirList.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let dirs = [];
+</script>
+
+<ul>
+  {#each dirs as dir}
+    <li>
+      > <a
+        class="text-blue-600 hover:underline hover:text-blue-400"
+        href="/{dir}">{dir}</a
+      >
+    </li>
+  {/each}
+</ul>
